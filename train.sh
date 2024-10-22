2f5b52a1d1b072a03750fd15a84105774f29516f
cc <49701208+choucaicai@users.noreply.github.com>
2024-10-22 10:48:29 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -4,5 +4,5 @@
 
 ACCELERATE_LOG_LEVEL=info accelerate launch \
     --config_file recipes/accelerate_configs/gpu1.yaml  scripts/run_drpo.py  \
-        recipes/qwen1.5-0.5b/qwen1.5-0.5-config_full_k8_diffndcg_lr5e-7_com+adam_prob_rank_0.2_adv2.yaml
+        recipes/xxxx.yaml
 
